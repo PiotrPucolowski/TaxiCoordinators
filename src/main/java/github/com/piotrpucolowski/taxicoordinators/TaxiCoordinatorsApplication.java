2d7c001a677de2d9877213f399adc9d3025d427f
Piotr Pucołowski <p.pucolowski@gmail.com>
2022-05-21 11:50:21 +0200

    Initial commit

---
+++ src/main/java/github/com/piotrpucolowski/taxicoordinators/TaxiCoordinatorsApplication.java
@@ -0,0 +1,13 @@
+package github.com.piotrpucolowski.taxicoordinators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiCoordinatorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiCoordinatorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/com/piotrpucolowski/taxicoordinators/TaxiCoordinatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.piotrpucolowski.taxicoordinators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiCoordinatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
