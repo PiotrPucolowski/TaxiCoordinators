912f2f194adb982a3b94a55bbf7a9d5b7166664a
Piotr Pucołowski <p.pucolowski@gmail.com>
2022-06-07 19:43:21 +0200

    CoordinatorRepository

--- src/main/java/github/com/piotrpucolowski/taxicoordinators/repository/CoordinatorRepository.java
+++ src/main/java/github/com/piotrpucolowski/taxicoordinators/repository/CoordinatorRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CoordinatorRepository extends JpaRepository<CoordinatorEntity, Long> {
 
-    //wyszukiwanie wszystkich upomnień po Imieniu i Nazwisku koordynatora albo po numerze koordynatora
-    List<CoordinatorEntity> findByFirstNameAndLastNameOrNumber(String firstName, String LastName, String number);
+    //wyszukiwanie wszystkich upomnień po numerze koordynatora
+    Optional<CoordinatorEntity> findByNumber( String number);
 
 }
